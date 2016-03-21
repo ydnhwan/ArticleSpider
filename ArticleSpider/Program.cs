bcb05bbdb67434c229d7c7b11381f5408ec270aa
hitme <yyhacker@gmail.com>
2016-03-21 22:32:37 +0800

    none
    none

---
+++ ArticleSpider/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleSpider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                GetWeb gb = new GetWeb();
+                gb.BasicUrl = "http://www.163.com";
+                gb.Start();
+        }
+    }
+}
